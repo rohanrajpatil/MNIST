21d11833e5b1e6ebc4b7a7066b01ae4db83d006e
rohanrajpatil <rohanraj_patil@berkeley.edu>
2025-10-16 01:18:23 -0700

    SGDclassifier

--- mnist.py
+++ mnist.py
@@ -1,6 +1,30 @@
 import numpy as np
+from numpy.random import RandomState
 import pandas as pd
-import sklearn as sk
 from sklearn.datasets import fetch_openml
 mnist = fetch_openml('mnist_784', as_frame=False)
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+x,y = mnist.data, mnist.target
+
+
+def plot(num):
+ image = num.reshape(28, 28)
+ plt.imshow(image, cmap="binary")
+ plt.axis("off")
+'''
+for i in range(5):
+    im = x[i]
+    w = plot(im)
+    plt.show()
+'''
+x_train,x_test,y_train, y_test = x[:6000], x[6000:], y[:6000], y[6000:]
+yt5 = (y_train =='5')
+ytt5 = (y_test == '5')
+
+
+from sklearn.linear_model import SGDClassifier
+sgd = SGDClassifier(random_state = 42)
+sgd.fit(x_train,yt5)
+x = sgd.predict([x[0]])
+print(x)
