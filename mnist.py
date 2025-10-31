ffc9d1d8a1e2302301707d9501b02209ea37a953
rohanrajpatil <rohanraj_patil@berkeley.edu>
2025-10-31 00:46:39 -0700

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -12,12 +12,12 @@ def plot(num):
  image = num.reshape(28, 28)
  plt.imshow(image, cmap="binary")
  plt.axis("off")
-'''
+
 for i in range(5):
     im = x[i]
     w = plot(im)
     plt.show()
-'''
+
 x_train,x_test,y_train, y_test = x[:6000], x[6000:], y[:6000], y[6000:]
 yt5 = (y_train =='5')
 ytt5 = (y_test == '5')
@@ -27,6 +27,7 @@ from sklearn.linear_model import SGDClassifier
 sgd = SGDClassifier(random_state = 42)
 sgd.fit(x_train,yt5)
 x = sgd.predict([x[0]])
+print(x)
 
 from sklearn.ensemble import GradientBoostingClassifier
 
@@ -45,8 +46,3 @@ p, r, t = precision_recall_curve(yt5, ysf)
 
 
 
-
-ypredfor = (ysf>=0.5)
-
-
-from sklearn.neighbors import KNeighborsClassifier
